b1a24a3c092d1ff83e412d40174f581f90cdbb23
Yurii Taran <yurii.taran@avenga.com>
2021-05-31 11:01:59 +0300

    update client

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -16,15 +16,15 @@ class App extends Component {
 
   render() {
     return (
-      <div className="container">
+      <div className='container'>
         <BrowserRouter>
           <div>
             <Header />
             <Switch>
-              <Route path="/blogs/new" component={BlogNew} />
-              <Route exact path="/blogs/:_id" component={BlogShow} />
-              <Route path="/blogs" component={Dashboard} />
-              <Route path="/" component={Landing} />
+              <Route path='/blogs/new' component={BlogNew} />
+              <Route exact path='/blogs/:_id' component={BlogShow} />
+              <Route path='/blogs' component={Dashboard} />
+              <Route path='/' component={Landing} />
             </Switch>
           </div>
         </BrowserRouter>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -15,28 +15,28 @@ class Header extends Component {
         );
       default:
         return [
-          <li key="3" style={{ margin: '0 10px' }}>
-            <Link to="/blogs">My Blogs</Link>
+          <li key='3' style={{ margin: '0 10px' }}>
+            <Link to='/blogs'>My Blogs</Link>
           </li>,
-          <li key="2">
+          <li key='2'>
             <a href={'/auth/logout'}>Logout</a>
-          </li>
+          </li>,
         ];
     }
   }
 
   render() {
     return (
-      <nav className="indigo">
-        <div className="nav-wrapper">
+      <nav className='indigo'>
+        <div className='nav-wrapper'>
           <Link
             to={this.props.auth ? '/blogs' : '/'}
-            className="left brand-logo"
+            className='left brand-logo'
             style={{ marginLeft: '10px' }}
           >
             Blogster
           </Link>
-          <ul className="right">{this.renderContent()}</ul>
+          <ul className='right'>{this.renderContent()}</ul>
         </div>
       </nav>
     );
