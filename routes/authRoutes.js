926f70e58d00360e8e0abdd2e72cec8587aec0d2
Yurii Taran <yurii.taran@avenga.com>
2021-05-31 11:07:06 +0300

    update config/dev.js (by prettier)

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 const passport = require('passport');
 
-module.exports = app => {
+module.exports = (app) => {
+  app.get('/auth', (req, res) => res.send('Auth route!!!'));
   app.get(
     '/auth/google',
     passport.authenticate('google', {
-      scope: ['profile', 'email']
+      scope: ['profile', 'email'],
     })
   );
 
