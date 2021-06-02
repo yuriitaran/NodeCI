fd0aac35da5e3acd2ef21b8b68f8aff5af6d3c35
Yurii Taran <yurii.taran@avenga.com>
2021-06-03 00:30:47 +0300

    add first test case

---
+++ tests/header.test.js
@@ -0,0 +1,17 @@
+const puppeteer = require('puppeteer');
+
+let browser, page;
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+    headless: false,
+  });
+  page = await browser.newPage();
+  await page.goto('localhost:3000');
+});
+
+test('We can launc a browser', async () => {
+  const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
+
+  expect(text).toEqual('Blogster');
+});
