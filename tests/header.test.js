b7de20b5dfb4b962e14641f3d5cf74d1dd48c3ce
Yurii Taran <yurii.taran@avenga.com>
2021-06-03 23:20:19 +0300

    add test case for logging in

--- tests/header.test.js
+++ tests/header.test.js
@@ -11,11 +11,43 @@ beforeEach(async () => {
 });
 
 afterEach(async () => {
-  await browser.close();
+  // await browser.close();
 });
 
-test('We can launc a browser', async () => {
+test('The header has the correct text', async () => {
   const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
 
   expect(text).toEqual('Blogster');
+});
+
+test('clicking login starts oauth flow', async () => {
+  await page.click('.right a');
+
+  const url = await page.url();
+  // console.log('url', url);
+
+  expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test.only('when signed in, shows logout button', async () => {
+  const Buffer = require('safe-buffer').Buffer;
+  const Keygrip = require('keygrip');
+  const keys = require('../config/keys');
+
+  const id = '60b89ce257929fd22ecbc08e';
+  const sessionObject = {
+    passport: {
+      user: id,
+    },
+  };
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
+    'base64'
+  );
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign('session=' + sessionString);
+
+  await page.setCookie({ name: 'session', value: sessionString });
+  await page.setCookie({ name: 'session.sig', value: sig });
+
+  await page.goto('localhost:3000');
 });
