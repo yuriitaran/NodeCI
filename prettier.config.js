2a010f0cd821823698bc7dc51f41931f73effbc4
Yurii Taran <elscept@gmail.com>
2021-06-08 18:56:42 +0300

    add prettier.config.js

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  trailingComma: 'none', // "<es5|none|all>"
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  arrowParens: 'avoid' // <"always|avoid">
+};
