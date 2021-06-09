56530a552990f379e3bc75277ab7e143cb71caee
Yurii Taran <elscept@gmail.com>
2021-06-09 16:06:44 +0300

    add getContentsOf method to Page class

--- tests/header.test.js
+++ tests/header.test.js
@@ -14,7 +14,7 @@ afterEach(async () => {
 });
 
 test('The header has the correct text', async () => {
-  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+  const text = await page.getContentsOf('a.brand-logo');
 
   expect(text).toEqual('Blogster');
 });
@@ -31,7 +31,7 @@ test('clicking login starts oauth flow', async () => {
 test('when signed in, shows logout button', async () => {
   await page.login();
 
-  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+  const text = await page.getContentsOf('a[href="/auth/logout"]');
 
   expect(text).toEqual('Logout');
 });
--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -29,6 +29,10 @@ class Page {
     await this.page.goto('localhost:3000');
     await this.page.waitFor('a[href="/auth/logout"]');
   }
+
+  async getContentsOf(selector) {
+    return this.page.$eval(selector, el => el.innerHTML);
+  }
 }
 
 module.exports = Page;
