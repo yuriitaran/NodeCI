f3c1bfb904afb5baef712f5c68733a84740b7677
Yurii Taran <elscept@gmail.com>
2021-06-18 09:29:41 +0300

    update prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  trailingComma: 'none', // "<es5|none|all>"
+  trailingComma: 'es5', // "<es5|none|all>"
   tabWidth: 2,
   semi: true,
   singleQuote: true,
-  arrowParens: 'avoid' // <"always|avoid">
+  arrowParens: 'always', // <"always|avoid">
 };
